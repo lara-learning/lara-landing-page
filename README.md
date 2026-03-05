113f3cde4d5e996b1b1233f217ced4198e0a7408
ArtemKharazian <artem.kharazian@accenture.com>
2026-03-05 12:10:07 +0100

    Initial commit
    Install Eleventy, Vite, Tailwind
    
    Install 11ty, Vite, Tailwind and add basic project structure
    
    feat: use vite for dev and build and test of pipelone for gh-pages deployment
    
    feat: use react templates with tailwind
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages
    
    fix: css 404 on gh-pages

---
+++ README.md
@@ -0,0 +1,3 @@
+# lara-landing-page
+
+This is an introductory landing page designed to provide a clear overview of what Lara is and how it works. It presents the core concept, main features, and overall structure of the system in a concise and accessible format.
---
+++ eleventy.config.js
@@ -0,0 +1,34 @@
+import "tsx/esm";
+
+export default async function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  const EleventyPluginVite = (await import("@11ty/eleventy-plugin-vite"))
+    .default;
+  const tailwindcss = (await import("@tailwindcss/vite")).default;
+
+  const isProd = process.env.NODE_ENV === "production";
+
+  eleventyConfig.addPlugin(EleventyPluginVite, {
+    viteOptions: {
+      clearScreen: false,
+      appType: "mpa",
+      plugins: [tailwindcss()],
+      root: "src",
+      base: isProd ? "/lara-landing-page/" : "/",
+    },
+  });
+
+  eleventyConfig.addExtension(["11ty.ts", "11ty.tsx"], {
+    key: "11ty.js",
+  });
+
+  eleventyConfig.addTemplateFormats("11ty.ts,11ty.tsx");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+}
