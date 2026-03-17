adf97729c6234604802d379b54c358dd3f3e5b2e
alexander-schefe <alexander.schefe@accenture.com>
2026-03-17 17:13:05 +0100

    feat: modified stage glow effect

--- src/page-fragments/stage.tsx
+++ src/page-fragments/stage.tsx
@@ -2,6 +2,7 @@ export default function () {
   return (
     <div className="relative -z-3 min-h-[900px] overflow-hidden bg-black">
       <div className="absolute -z-2 pointer-events-none bottom-[20%] left-[25%] w-[550px] h-[550px] bg-[#da67cf] rounded-full blur-[160px] opacity-100 mix-blend-screen"></div>
+      <div className="absolute -z-2 pointer-events-none bottom-[20%] right-[15%] w-[550px] h-[550px] bg-[#DA1C85] rounded-full blur-[160px] opacity-100 mix-blend-screen"></div>
 
       <Glow1 />
       <Glow1 />
@@ -12,15 +13,15 @@ export default function () {
       <Glow3 />
       <Glow3 />
 
-      <div className="mx-32 flex items-center flex-col">
+      <div className="mx-16 md:mx-32 lg:mx-64 flex items-center flex-col">
         <img
           className="w-[60px] aspect-1 mt-[140px]"
           src="/assets/svg/logo.svg"
         />
-        <h1 className="text-white text-8xl font-bold text-center mt-4">
+        <h1 className="text-white text-6xl lg:text-8xl font-bold text-center mt-4">
           Willkommen bei lara!
         </h1>
-        <p className="text-white text-xl text-center mt-6">
+        <p className="text-white text-l lg:text-xl text-center mt-6">
           Lara ist ein digitales Berichtsheft, welches von Azubis für Azubis
           entwickelt wurde. Mit Lara hast Du die Möglichkeit, dein Berichtsheft
           ganz einfach und ohne Probleme digital zu führen.
